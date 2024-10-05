5a354aa7bfd403319e85a1c9d90589c6907c91ae
ArsalanSikander <arsalansikander0216@gmail.com>
2024-10-06 01:52:21 +0500

    Made it work with local mongoDB
    Just changed the database mongoURL so that it uses a local db.

--- New folder/Backend/config.js
+++ New folder/Backend/config.js
@@ -1,4 +1,5 @@
 export const PORT = 5555;
 
-export const MONGODBURL =
+export const MONGODBURLOLD =
   "mongodb+srv://ecoding45:easycoding@cluster0.akkebzk.mongodb.net/Book?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODBURL = "mongodb://localhost:27017/Library"
